feb341f28825c82918d2a4eaa6f70001ecbcac20
Preston Massey-Blake <pmassey-blake@mail.csuchico.edu>
2023-01-03 16:47:42 -0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include <set>
+#include <clang/ASTMatchers/ASTMatchers.h>
+#include <clang/ASTMatchers/ASTMatchFinder.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
+
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace clang::tooling;
+
+struct MyStruct
+{
+    std::string name;
+    DeclarationMatcher declMatcher;
+    MyStruct(const std::string &Name) : name(Name), declMatcher(functionDecl(hasName(Name)).bind("functionDecl")) {}
+};
+
+class MyStructHandler : public MatchFinder::MatchCallback
+{
+public:
+    MyStructHandler(const MyStruct &Struct) : myStruct(Struct) {}
+
+    virtual void run(const MatchFinder::MatchResult &Result)
+    {
+        if (const FunctionDecl *functionDecl = Result.Nodes.getNodeAs<FunctionDecl>("functionDecl"))
+        {
+           
+        }
+    }
+
+private:
+    const MyStruct &myStruct;
+};
+
+int main(int argc, const char **argv)
+{
+    CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
+    ClangTool Tool(OptionsParser.getCompilations(),
+                   OptionsParser.getSourcePathList());
+
+    std::set<MyStruct> structSet;
+
+    MyStruct struct1("one");
+    MyStruct struct2("two");
+
+    structSet.insert(struct1);
+    structSet.insert(struct2);
+
+    MatchFinder finder;
+    for (const MyStruct &myStruct : structSet)
+    {
+        MyStructHandler handler(myStruct);
+        finder.addMatcher(myStruct.declMatcher, &handler);
+    }
+
+    return Tool.run(newFrontendActionFactory(&finder).get());
+}
